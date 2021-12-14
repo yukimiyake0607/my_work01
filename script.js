ab4c1c3691fc1ecbd5d2bad87f27c153370597f8
yukimiyake0607 <yukimiyake38@gmail.com>
2021-12-14 18:41:27 +0900

    選択した要素を表示する

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ $( function() {
 
     // menuの最初の要素以外を非表示にする
     $('.contents > div[id != "lunch"]').hide();
+
+    $('.tab-menu__list-item > a').on('click', function() {
+
+        // 1度全ての要素を非表示にする
+        $('.contents > .menu').hide();
+
+        // 選択された要素を表示する
+        const a = $(this).attr ("href");
+        $(a).show();
+
+        return false;
+    })
 });
